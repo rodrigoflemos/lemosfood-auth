d8b896f5707163a4d6416d3e8c2dad21af21a056
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-21 18:07:39 -0300

    Aula 22.8
    Criando o projeto do Authorization Server com Spring Security OAuth2

---
+++ src/main/java/br/com/lemos/lemosfood/auth/LemosfoodAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.lemos.lemosfood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemosfoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemosfoodAuthApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/lemos/lemosfood/auth/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.lemos.lemosfood.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		//Configura usuario em memoria
+		
+		auth.inMemoryAuthentication()
+			.withUser("rodrigo")
+				.password(passwordEncoder().encode("123456"))
+				.roles("ADMIN")
+			.and()
+			.withUser("joao")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN");
+	}
+	
+	//Bean registrado para que a configuracao de usuario em memoria funcione. Caso nao seja registrado ocorre uma exception apontando id null
+	@Bean
+	public PasswordEncoder passwordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
+}
