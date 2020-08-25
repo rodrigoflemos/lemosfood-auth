71f8f94f317061059f670f242103a81e214db760
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-25 10:25:59 -0300

    Aula 23.5
    Gerando JWT com chave simétrica (HMAC SHA-256) no Authorization Server

--- src/main/java/br/com/lemos/lemosfood/auth/AuthorizationServerConfig.java
+++ src/main/java/br/com/lemos/lemosfood/auth/AuthorizationServerConfig.java
@@ -3,7 +3,9 @@ package br.com.lemos.lemosfood.auth;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +16,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -28,6 +31,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
+		
 	//Configura os detalhes dos clients 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -96,7 +102,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
 			.reuseRefreshTokens(false)
+			.accessTokenConverter(jwtAccessTokenConverter())
 			.tokenGranter(tokenGranter(endpoints));// Gera um novo refresh token cada vez que o refresh token for utilizado
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter () {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("rodrigoflemos");
+		return jwtAccessTokenConverter;
 	}
 	
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
