4b0caadb863f47f0f5e79ad9426b7784954f16c9
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-26 12:43:07 -0300

    Aula 23.11
    Extraindo a chave pública no formato PEM

--- src/main/java/br/com/lemos/lemosfood/auth/AuthorizationServerConfig.java
+++ src/main/java/br/com/lemos/lemosfood/auth/AuthorizationServerConfig.java
@@ -97,6 +97,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
 		security.checkTokenAccess("permitAll()")
+			.tokenKeyAccess("permitAll()")
 			.allowFormAuthenticationForClients();
 	}
 	
