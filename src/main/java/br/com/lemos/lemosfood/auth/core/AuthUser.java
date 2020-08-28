8f6609e1dd4a064be65448e83c77cc6e3151aca6
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-28 10:04:18 -0300

    Aula 23.19
    Carregando as permissões concedidas na autenticação

--- src/main/java/br/com/lemos/lemosfood/auth/core/AuthUser.java
+++ src/main/java/br/com/lemos/lemosfood/auth/core/AuthUser.java
@@ -1,7 +1,9 @@
 package br.com.lemos.lemosfood.auth.core;
 
+import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import br.com.lemos.lemosfood.auth.domain.Usuario;
@@ -15,8 +17,8 @@ public class AuthUser extends User{
 	private Long userId;
 	private String fullName;
 	
-	public AuthUser(Usuario usuario) {
-		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+	public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
 		
 		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
--- src/main/java/br/com/lemos/lemosfood/auth/core/JpaUserDetailsService.java
+++ src/main/java/br/com/lemos/lemosfood/auth/core/JpaUserDetailsService.java
@@ -1,10 +1,16 @@
 package br.com.lemos.lemosfood.auth.core;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.lemos.lemosfood.auth.domain.Usuario;
 import br.com.lemos.lemosfood.auth.domain.UsuarioRepository;
@@ -15,13 +21,22 @@ public class JpaUserDetailsService implements UserDetailsService{
 	@Autowired
 	UsuarioRepository usuarioRepository;
 	
+	@Transactional(readOnly = true)
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		Usuario usuario = usuarioRepository.findByEmail(username)
 				.orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail informado"));
 		
-		return new AuthUser(usuario);
+		return new AuthUser(usuario, getAuthorities(usuario));
 	}
 
+	private Collection<GrantedAuthority> getAuthorities (Usuario usuario){
+		
+	
+		return usuario.getGrupos().stream()
+				.flatMap(grupo -> grupo.getPermissoes().stream())
+				.map(permissao -> new SimpleGrantedAuthority(permissao.getNome().toUpperCase()))
+				.collect(Collectors.toSet());
+	}
 }
